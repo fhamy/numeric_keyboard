8cedf13b041a50924b1ae8c7cf90194aeb25f537
fhamy <fhamy@twiscoder.com>
2022-03-07 12:39:45 +0700

    Custom numeric keyboard

--- lib/numeric_keyboard.dart
+++ lib/numeric_keyboard.dart
@@ -5,12 +5,25 @@ import 'package:flutter/material.dart';
 typedef KeyboardTapCallback = void Function(String text);
 
 class NumericKeyboard extends StatefulWidget {
+  /// Color of the text [default = Colors.black]
   final Color textColor;
+
+  /// Display a custom right icon
   final Icon rightIcon;
+
+  /// Action to trigger when right button is pressed
   final Function() rightButtonFn;
+
+  /// Display a custom left icon
   final Icon leftIcon;
+
+  /// Action to trigger when left button is pressed
   final Function() leftButtonFn;
+
+  /// Callback when an item is pressed
   final KeyboardTapCallback onKeyboardTap;
+
+  /// Main axis alignment [default = MainAxisAlignment.spaceEvenly]
   final MainAxisAlignment mainAxisAlignment;
 
   NumericKeyboard(
@@ -95,17 +108,23 @@ class _NumericKeyboardState extends State<NumericKeyboard> {
         onTap: () {
           widget.onKeyboardTap(value);
         },
-        child: Container(
-          alignment: Alignment.center,
-          width: 50,
-          height: 50,
-          child: Text(
-            value,
-            style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-                color: widget.textColor),
-          ),
-        ));
+        child: Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Container(
+              alignment: Alignment.center,
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 1, color: widget.textColor)
+              ),
+              child: Text(
+                value,
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color:  widget.textColor),
+              ),
+            )));
   }
 }
